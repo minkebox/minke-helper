724f2ca270859409b5ac6d1eaea7f88a196682e6
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-02 13:14:42 -0800

    Emit status events

--- overlay/startup.sh
+++ overlay/startup.sh
@@ -14,6 +14,7 @@ fi
 if [ "${ENABLE_DHCP}" != "" ]; then
   udhcpc -R -i ${IFACE} -s /udhcpc.script -F ${HOSTNAME} -x hostname:${HOSTNAME}
   IP=$(ip addr show dev ${IFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
+  echo "MINKE:DHCP:UP ${IFACE} ${IP}"
 fi
 
 up()
@@ -24,6 +25,7 @@ up()
       port=${map%%:*}
       protocol=${map#*:}
       upnpc -a ${IP} ${port} ${port} ${protocol} ${TTL}
+      echo "MINKE:NAT:UP ${IP} ${port} ${protocol} ${TTL}"
     done
   fi
 }
@@ -36,17 +38,23 @@ down()
       port=${map%%:*}
       protocol=${map#*:}
       upnpc -d ${port} ${protocol}
+      echo "MINKE:NAT:DOWN ${IP} ${port} ${protocol}"
     done
   fi
   if [ "${ENABLE_DHCP}" != "" ]; then
     killall udhcpc
+    echo "MINKE:DHCP:DOWN ${IFACE} ${IP}"
   fi
 }
 
 trap "down; killall sleep; exit" TERM INT
 
+up
+
+echo "MINKE:UP"
+
 while true; do
-  up
   sleep ${TTL2} &
   wait "$!"
+  up
 done
