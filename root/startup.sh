b4627db56c62dd17e786129c0e30c8d707e3efc7
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-02-05 00:47:12 -0800

    Better UPNPURL handling

--- root/startup.sh
+++ root/startup.sh
@@ -68,6 +68,12 @@ echo "search ${__DOMAINNAME}. local.
 nameserver ${__DNSSERVER}
 options ndots:1 timeout:1 attempts:1 ndots:0" > /etc/resolv.conf
 
+if [ "${__UPNPURL}" != "" ]; then
+  UPNPRUL="-u ${__UPNPURL}"
+else
+  UPNPURL=""
+fi
+
 up()
 {
   if [ "${ENABLE_NAT}" != "" ]; then
@@ -75,9 +81,9 @@ up()
       # port:protocol
       port=${map%%:*}
       protocol=${map#*:}
-      upnpc -u ${__UPNPURL} -e ${HOSTNAME} -m ${PRIMARY_INTERFACE} -n ${PRIMARY_IP} ${port} ${port} ${protocol} ${TTL}
+      upnpc ${UPNPURL} -e ${HOSTNAME} -m ${PRIMARY_INTERFACE} -n ${PRIMARY_IP} ${port} ${port} ${protocol} ${TTL}
       if [ "${PRIMARY_IP6}" != "" ]; then
-        upnpc -u ${__UPNPURL} -e ${HOSTNAME}_6 -m ${PRIMARY_INTERFACE} -6 -A "" 0 ${PRIMARY_IP6} ${port} ${protocol} ${TTL}
+        upnpc ${UPNPURL} -e ${HOSTNAME}_6 -m ${PRIMARY_INTERFACE} -6 -A "" 0 ${PRIMARY_IP6} ${port} ${protocol} ${TTL}
       fi
       echo "MINKE:NAT:UP ${PRIMARY_IP} ${port} ${protocol} ${TTL}"
     done
@@ -91,9 +97,9 @@ reup()
       # port:protocol
       port=${map%%:*}
       protocol=${map#*:}
-      upnpc -u ${__UPNPURL} -e ${HOSTNAME} -m ${PRIMARY_INTERFACE} -n ${PRIMARY_IP} ${port} ${port} ${protocol} ${TTL}
+      upnpc ${UPNPURL} -e ${HOSTNAME} -m ${PRIMARY_INTERFACE} -n ${PRIMARY_IP} ${port} ${port} ${protocol} ${TTL}
       if [ "${PRIMARY_IP6}" != "" ]; then
-        upnpc -u ${__UPNPURL} -e ${HOSTNAME}_6 -m ${PRIMARY_INTERFACE} -6 -A "" 0 ${PRIMARY_IP6} ${port} ${protocol} ${TTL}
+        upnpc ${UPNPURL} -e ${HOSTNAME}_6 -m ${PRIMARY_INTERFACE} -6 -A "" 0 ${PRIMARY_IP6} ${port} ${protocol} ${TTL}
       fi
     done
   fi
@@ -106,7 +112,7 @@ down()
       # port:protocol
       port=${map%%:*}
       protocol=${map#*:}
-      upnpc -u ${__UPNPURL} -m ${PRIMARY_INTERFACE} -d ${port} ${protocol}
+      upnpc ${UPNPURL} -m ${PRIMARY_INTERFACE} -d ${port} ${protocol}
       echo "MINKE:NAT:DOWN ${PRIMARY_IP} ${port} ${protocol}"
     done
   fi
